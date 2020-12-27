bbdff36c68d1bde2861c002f2ad22cd71cc2fcd2
evgeniiy28 <alexeevich.92@mail.ru>
2020-12-27 15:26:11 +0300

    test

---
+++ testt/testt/2/prog2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testt._2
+{
+    static void Main(string[] args)
+    {
+        int[,] nums = new int[10, 10];
+        Random rnd = new Random();
+        int sum = 0;
+        for (int i=0; i<10; i++)
+        {
+            for (int j=0; j<10; j++)
+            {
+                nums[i, j] = rnd.Next(100);
+                Console.Write(nums[i, j] + " ");
+                if (i == j)
+                {
+                    sum += nums[i, j];
+                }
+            }
+            
+            Console.WriteLine();
+        }
+        Console.WriteLine(sum);
+    }
+}
