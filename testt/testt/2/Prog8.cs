1279bb4704be4e7f5d2954c757ed14e3a624bb3f
evgeniiy28 <alexeevich.92@mail.ru>
2020-12-28 17:27:56 +0300

    tst com

---
+++ testt/testt/2/Prog8.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test_project3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero hero1 = new Hero("Герой", 1000, 50, 10, 30);
+            Hero hero2 = new Hero("Самозванник", 600, 10, 50, 50);
+            hero1.Damage(hero2);
+
+        }
+    }
+}
